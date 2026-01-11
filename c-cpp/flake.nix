6403ce2c5c4cf75fdbebff65c006fecee2530878
kmboyle <>
2026-01-11 18:49:15 -0500

    not used

--- c-cpp/flake.nix
+++ c-cpp/flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # stable Nixpkgs
 
   outputs =
-    { self, ... }@inputs:
+    { ... }@inputs:
 
     let
       supportedSystems = [
