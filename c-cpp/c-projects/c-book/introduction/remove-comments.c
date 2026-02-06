5315ad6da8f6fe1960cf6045000e4c4dd080767c
kmboyle <>
2026-02-06 22:02:42 +0000

    comments

--- c-cpp/c-projects/c-book/introduction/remove-comments.c
+++ c-cpp/c-projects/c-book/introduction/remove-comments.c
@@ -32,7 +32,7 @@ void remove_comments(char line[], char removed_comments[]) {
 
   int i, j;
   int column = 0;
-
+  int inside = 0;
   for (i = 0, j = 0; line[i] != '\0'; ++i, ++j) {
     // removed_comments[j] = line[i];
 
@@ -41,7 +41,15 @@ void remove_comments(char line[], char removed_comments[]) {
     }
     column++;
 
-    if (line[j] != '/' && line[j++] != '/') {
+    if (line[j] == '/' && line[j + 1] == '*') {
+      inside = j + 1;
+      while (line[++inside] != '*' && getchar() != EOF) {
+        j++;
+        continue;
+      }
+      removed_comments[j] = line[i];
+    }
+    if (line[j] != '/' && line[j + 1] != '/') {
       removed_comments[j] = line[i];
     }
   }
